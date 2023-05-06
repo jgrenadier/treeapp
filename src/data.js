2940b91545c5f47ac3b32f2d40e661f4f4c9312d
Jim Grenadier <jgrendier@gmail.com>
2023-05-05 21:21:45 -0500

    Configure React app for deployment to GitHub Pages

---
+++ src/data.js
@@ -0,0 +1,107 @@
+const data = {
+    name: 'States',
+    children: [
+      {
+        name: 'Texas',
+        children: [
+          {
+            category: 'Texas',
+            name: 'Silver Maple',
+            value: 20.4,
+          },
+          {
+            category: 'Texas',
+            name: 'ChinaBerry',
+            value: 12.9,
+          },
+          {
+            category: 'Texas',
+            name: 'Oak',
+            value: 6.9,
+          },
+          {
+            category: 'Texas',
+            name: 'Sugar Maple',
+            value: 3.6,
+          },
+          {
+            category: 'Texas',
+            name: 'Juniper',
+            value: 3.3,
+          },
+          {
+            category: 'Texas',
+            name: 'Trash',
+            value: 2.5,
+          },
+        ],
+      },
+      {
+        name: 'Oregon',
+        children: [
+          {
+            category: 'Oregon',
+            name: 'Pine',
+            value: 23.4,
+          },
+          {
+            category: 'Oregon',
+            name: 'Laurel',
+            value: 20.3,
+          },
+          {
+            category: 'Oregon',
+            name: 'Mountain Laurel',
+            value: 17.5,
+          },
+          {
+            category: 'Oregon',
+            name: 'Juniper',
+            value: 6.4,
+          },
+          {
+            category: 'Oregon',
+            name: 'Family',
+            value: 7.4,
+          },
+          {
+            category: 'Oregon',
+            name: 'Shoe',
+            value: 3.4,
+          },
+          {
+            category: 'Oregon',
+            name: 'Coconut',
+            value: 12.9,
+          },
+        ],
+      },
+      {
+        name: 'Hawaii',
+        children: [
+          {
+            category: 'Hawaii',
+            name: 'Coconut',
+            value: 9.2,
+          },
+          {
+            category: 'Hawaii',
+            name: 'Willow',
+            value: 8.1,
+          },
+          {
+            category: 'Hawaii',
+            name: 'Redwood',
+            value: 5.2,
+          },
+          {
+            category: 'Hawaii',
+            name: 'White Oak',
+            value: 3.3,
+          },
+        ],
+      },
+    ],
+  };
+  
+  export default data;
